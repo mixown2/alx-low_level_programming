9ef71e4dbac4ffdc6943bcf3e5598e93463918d3
mixown2 <mixown@gmail.com>
2022-07-01 14:53:09 -0700

    giggou

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,13 @@
  * main - Prints the alphabetic
  *
  * Return: Always (0)
- * */
+ */
 int main(void)
 {
 	char c;
 	
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-	putchar('\n');
-	
-	return (0);
+	putchar('\n')
+		return (0);
 }
