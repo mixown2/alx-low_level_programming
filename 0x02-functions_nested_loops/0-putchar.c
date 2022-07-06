03a834ad88b6744012c9e8d63c58dca96db4d38d
mixown2 <94745688+mixown2@users.noreply.github.com>
2022-07-06 03:21:55 +0300

    put char.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *main - entry point
+ *
+ *Description: prints _putchar followed by a newline
+ *
+ *Return: returns 0(successful)
+ *
+ */
+int main(void)
+{
+	char putchartext[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(putchartext[i]);
+	}
+	_putchar('\n');
+	return (0);
+
+}
