92c842703c0b21068a205e97bb4ae53c4950b700
mixown2 <94745688+mixown2@users.noreply.github.com>
2022-08-03 23:53:36 +0300

    mm

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,32 +1,21 @@
-#include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
-
+#ifndef _HEADER_
+#define _HEADER_
 /**
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
-int (*get_op_func(char *s))(int, int)
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
 {
-	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
-	};
-	int i = 0;
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
-	while (i < 5)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-
-		i++;
-	}
-
-	return (0);
-}
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
